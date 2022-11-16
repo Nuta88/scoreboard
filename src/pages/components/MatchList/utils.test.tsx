bebc2b43c8c4fcf3acbc523001d63facd41ec569
Anna Wagner <annwagner8888@gmail.com>
2022-11-16 10:20:02 +0200

    scoreboard-feature: Add test for sorting

---
+++ src/pages/components/MatchList/utils.test.tsx
@@ -0,0 +1,25 @@
+import { calculateTotalScore, scoreTimeMatchComparator, sortMatches } from './utils';
+
+import { Match } from '../../types';
+
+describe("Utils test", () => {
+  const match1 = new Match('Canada', 'Mexico', 2, 5);
+  const match2 = new Match('Spain', 'Brazil', 2, 10);
+  
+  
+  test('should calculate total match score', () => {
+    expect(calculateTotalScore(match1)).toEqual(7);
+  });
+  
+  test('should compare matches', () => {
+    const result = 5;
+    
+    expect(scoreTimeMatchComparator(match1, match2)).toEqual(result);
+  });
+  
+  test('should sort matches', () => {
+    const result = [match2, match1];
+    
+    expect(sortMatches([match1, match2])).toEqual(result);
+  });
+});
