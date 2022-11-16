a32f6b4d93e38ea13efa827d70c8f94c01269471
Anna Wagner <annwagner8888@gmail.com>
2022-11-16 10:23:39 +0200

    scoreboard-feature: Add sorting for matches

---
+++ src/pages/components/MatchList/utils.ts
@@ -0,0 +1,8 @@
+import { IMatch } from '../../types';
+
+export const calculateTotalScore = (match: IMatch) => match.homeTeamPoints + match.awayTeamPoints;
+
+export const scoreTimeMatchComparator = (matchA: IMatch, matchB: IMatch) =>
+  (calculateTotalScore(matchB) - calculateTotalScore(matchA)) || (matchB.startTime - matchA.startTime);
+
+export const sortMatches = (matches: IMatch[]) => matches.sort(scoreTimeMatchComparator);
