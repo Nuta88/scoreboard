1bbf917132a74f1ede1777773a17f72703b0c634
Anna Wagner <annwagner8888@gmail.com>
2022-11-16 11:51:56 +0200

    scoreboard-feature: Add match list to main component

--- src/pages/App.test.tsx
+++ src/pages/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/app/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component", () => {
+  test('page should have v and card', () => {
+    render(<App />);
+    
+    expect(screen.getByTestId('board-container')).toBeInTheDocument();
+    expect(screen.getByTestId('board-container-card')).toBeInTheDocument();
+  });
+  test('should open create match modal', async () => {
+    const { getByTestId } = render(<App />);
+    
+    expect(getByTestId('board-add-match-btn')).toBeInTheDocument();
+    fireEvent.click(getByTestId('board-add-match-btn'));
+    
+    await waitFor(() => expect(getByTestId('create-match-modal')).toBeInTheDocument());
+  });
 });
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react';
+import { useState } from 'react';
+
+import { Card } from 'antd';
+
 import './App.css';
+import MatchList from './components/MatchList';
+
+import { IMatch } from '../types';
+
+const cardStyle = {
+  headStyle: { backgroundColor: '#b25949' },
+  bodyStyle: { backgroundColor: '#3f4464' },
+};
 
 function App() {
+  const [matches, setMatches] = useState<IMatch[]>([]);
+  
+  const handleDelete = (id: string) => {
+    setMatches(matches => matches.filter(matche => matche.id !== id));
+  };
+  
+  const handleUpdateMatch = (updatesMatch: IMatch) => {
+    setMatches(matches => matches.map(match => match.id === updatesMatch.id ? updatesMatch : match));
+  };
+  
   return (
-    <div className="App">
-      app
+    <div
+      data-testid="board-container"
+      className="board-container"
+    >
+      <Card
+        title="TODAY'S MATCHES"
+        data-testid="board-container-card"
+        bordered={false}
+        {...cardStyle}
+      >
+        <MatchList
+          matches={matches}
+          onDelete={handleDelete}
+          onUpdate={handleUpdateMatch}
+        />
+      </Card>
     </div>
   );
 }
